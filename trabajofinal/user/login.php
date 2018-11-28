cacdb9ccd1d116b336c183d3efd441a2b35c74f2
jerotaber <jerotaber@gmail.com>
2018-11-28 15:50:54 -0300

    Tokens login & validation

--- trabajofinal/user/login.php
+++ trabajofinal/user/login.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-require_once '../vendor/autoload.php';
+require_once '../libs/vendor/autoload.php';
 use \Firebase\JWT\JWT;
 // files needed to connect to database
 include_once '../config/database.php';
--- trabajofinal/user/validate_token.php
+++ trabajofinal/user/validate_token.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once '../vendor/autoload.php';
+require_once '../libs/vendor/autoload.php';
 use \Firebase\JWT\JWT;
 include_once '../config/core.php';
 // get posted data
